f33592e4dc7e2e4304e1cd3cf4ceed3a8d6bb2a4
Yang Bai <cedriccan48@gmail.com>
2019-07-22 17:28:10 -0400

    Updates

---
+++ precache-manifest.ed005b484e99f3451ed4e49d098d2fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5108d067e913277e4dc2b24f7d01cc87",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "41dfd5b2947c5c50a2f0",
+    "url": "/monsters-rolodex/static/css/main.0aa68adc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "41dfd5b2947c5c50a2f0",
+    "url": "/monsters-rolodex/static/js/main.da6d3a09.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
